4d490dade148c54440d76c72fc5ee81f8b441692
Cyrille Varin <cyrille.varin@live.fr>
2022-11-28 16:43:48 +0100

    first commit

--- model.py
+++ model.py
@@ -1,1 +1,22 @@
-#fcgfgfgdf
+#model test line
+
+"""
+This is a test
+
+"""
+
+List of model to test :
+
+tf_efficientnet_l2_ns	88.35%	98.66%
+tf_efficientnet_b7_ns	86.83%	98.08%
+tf_efficientnet_b6_ns	86.45%	97.88%
+tf_efficientnet_b5_ns	86.08%	97.75%
+tf_efficientnet_b4_ns	85.15%	97.47%
+tf_efficientnet_b3_ns	84.04%	96.91%
+tf_efficientnet_b2_ns	82.39%	96.24%
+tf_efficientnet_b1_ns	81.39%	95.74%
+tf_efficientnet_b0_ns
+
+List to tester :
+    Adam
+    softmax
